921f82578c8c7b74459371c2bdd43004d205e330
lidia <lidiapshenichnay@yandex.ru>
2021-04-09 17:15:42 +0300

    исправления

--- src/main/Find.java
+++ src/main/Find.java
@@ -1,13 +1,15 @@
 package main;
 
+import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
 /*
 Вариант 4 — find
 
 Поиск файла(ов) с заданным в командной строке именем в указанной ключом -d директории,
 по умолчанию в текущей директории. Ключ -r указывает на необходимость поиска также во всех поддиректориях.
 
-Command Line: find [-r] [-d directory] filename.txt
+Command Line: [-r] [-d directory] filename.txt
 
 Кроме самой программы, следует написать автоматические тесты к ней.
  */
@@ -15,60 +17,58 @@ Command Line: find [-r] [-d directory] filename.txt
 public class Find {
     public static void main(String[] args) {
         if (args.length < 1 || args.length > 4) {
-            System.err.println("Illegal Console Argument");
-            return;
+            System.exit(1);
         }
         int i = 0;
         boolean flagRec = false;
-        if (args[i].equals("-r")) {
-            flagRec = true;
-            i++;
-        }
         String directory = ".";
-        if (args[i].equals("-d")) {
-            if (args.length < i + 3) {
-                System.err.println("Illegal Console Argument");
-                return;
-            }
-            directory = args[i + 1];
-            i += 2;
-        }
-        int length = args[i].length();
-        if (length < 5) {
-            System.err.println("Illegal Console Argument");
-            return;
-        }
         String filename = "";
-        if (!args[i].endsWith(".txt")) {
-            System.err.println("Illegal Console Argument");
-            return;
-        } else {
-            filename = args[i];
+
+        for(; i < args.length; i++) {
+            if (args[i].equals("-r")) {
+                flagRec = true;
+            } else if (args[i].equals("-d")) {
+                if (args.length < i + 3) {
+                    System.exit(1);
+                }
+                directory = args[i + 1];
+                i++;
+            } else {
+                int length = args[i].length();
+                if (length < 5) {
+                    System.exit(1);
+                }
+                filename = args[i];
+            }
         }
 
-        String result = findFile(filename, directory, flagRec);
+        ArrayList<String> result = findFile(filename, directory, flagRec);
         if (!result.equals("")) System.out.println(result);
                      else System.out.println("File \""+filename+ "\" not found");
     }
 
-    public static String findFile(String fileName, String directory, boolean flagRec) {
+    public static ArrayList<String> findFile(String fileName, String directory, boolean flagRec) {
+        ArrayList<String> paths = new ArrayList<>();
         File dir = new File(directory);
         File[] filesInDirectory = dir.listFiles();
-        if (filesInDirectory == null) return "";
+        if (filesInDirectory == null) return paths;
         for (File i : filesInDirectory) {
             if (i.isFile())
-              if (fileName.equals(i.getName()))
-                return directory + File.separator + fileName;
+              if (fileName.equals(i.getName())) {
+                  paths.add(i.getPath());
+              }
         }
         if (flagRec) {
             for (File i : filesInDirectory) {
                 if (i.isDirectory()) {
-                    String res = findFile(fileName, directory + File.separator + i.getName(), flagRec);
-                    if (!res.equals("")) return res;
+                    ArrayList<String> res = findFile(fileName, directory + File.separator + i.getName(), flagRec);
+                    if (res.size() > 0){
+                        paths.addAll(res);
+                    }
                 }
             }
         }
-        return "";
+        return paths;
     }
 }
 
--- src/test/TestFindFile.java
+++ src/test/TestFindFile.java
@@ -5,19 +5,23 @@ import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
+import java.io.File;
+
 public class TestFindFile {
+
     // проверка собственно функции поиска
     @Test
     public void findFile() {
-        assertEquals("src\\testDir\\file1.txt",
-                Find.findFile("file1.txt","src\\testDir",false));
-        assertEquals("src\\testDir\\subdir1\\file2.txt",
-                Find.findFile("file2.txt","src\\testDir",true));
+        assertEquals("src" + File.separator + "testDir" + File.separator + "file1.txt",
+                Find.findFile("file1.txt","src" + File.separator + "testDir",false));
+        assertEquals("src" + File.separator + "testDir" + File.separator + "subdir1" + File.separator + "file2.txt",
+                Find.findFile("file2.txt","src" + File.separator + "testDir",true));
         assertEquals("",
-                Find.findFile("file2.txt","src\\testDir",false));
+                Find.findFile("file2.txt","src" + File.separator + "testDir",false));
     }
 
    // лог сохраняющий вывод в System.out
@@ -26,7 +30,8 @@ public class TestFindFile {
     // лог сохраняющий вывод в System.err
     @Rule
     public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
-
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
     @Test
     public void TestMain() {
         // тест при правильном поиске
@@ -34,26 +39,26 @@ public class TestFindFile {
         systemOutRule.clearLog();
         systemErrRule.clearLog();
         Find.main("-r -d src\\testDir file1.txt".split(" "));
-        assertEquals("src\\testDir\\file1.txt", systemOutRule.getLog().split("\r")[0]);
+        assertEquals("[src" + File.separator + "testDir" + File.separator + "file1.txt]", systemOutRule.getLog().split("\r")[0]);
 
         // тест при правильном поиске без рекурсии
         // очистка логов
         systemOutRule.clearLog();
         systemErrRule.clearLog();
         Find.main("file1.txt".split(" "));
-        assertEquals("File \"file1.txt\" not found", systemOutRule.getLog().split("\r")[0]);
+        assertEquals("[]", systemOutRule.getLog().split("\r")[0]);
 
         // тест при правильном поиске с рекурсией
         // очистка логов
         systemOutRule.clearLog();
         systemErrRule.clearLog();
         Find.main("-r file1.txt".split(" "));
-        assertEquals(".\\out\\production\\find\\testDir\\file1.txt", systemOutRule.getLog().split("\r")[0]);
+        assertEquals("[." + File.separator + "out" + File.separator + "production" + File.separator + "find" + File.separator + "testDir" + File.separator + "file1.txt, ." + File.separator + "src" + File.separator + "testDir" + File.separator + "file1.txt]", systemOutRule.getLog().split("\r")[0]);
 
         // тест при неправильных параметрах
         systemOutRule.clearLog();
         systemErrRule.clearLog();
+        exit.expectSystemExit();
         Find.main("-v -d src\\testDir file1.txt".split(" "));
-        assertEquals("Illegal Console Argument", systemErrRule.getLog().split("\r")[0]);
-    }
+        }
 }
